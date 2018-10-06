136cf0b8d0245e4588d77beea196a00e2168e92f
a_maulana <cintamupalsu@hotmail.com>
2018-10-06 11:47:12 +0900

    undo

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/about',   to: 'common_pages#about'
   get '/contact', to: 'common_pages#contact'
   get '/signup',  to: 'user#new'
-  
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
